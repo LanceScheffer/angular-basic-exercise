76a7482fca948086321e4930e8ab37bcc4b7812c
LanceScheffer <lancescheffer@gmail.com>
2022-09-25 14:05:41 -0500

    Update article.component.ts

--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -6,7 +6,8 @@ import { Component } from '@angular/core';
   templateUrl: './article.component.html'
 })
 export class ArticleComponent {
-   title: string = "Whatever you want";
-   content: string = "Some content goes here";
+   title: string = "Important Info Below";
+   content: string = "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eligendi ducimus aut iure mollitia omnis impedit necessitatibus, molestiae et illo repellendus, amet nobis consequuntur inventore natus alias, nam distinctio a quam!";
+
    isTechRelated: boolean = true;
 }
